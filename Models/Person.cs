a79c3a86e2b31e3b9fd2171bcb9d478facbef1d4
Samuel <padmadansamuel@gmail.com>
2024-07-17 19:56:01 +0530

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculatorApp.Models
+{
+    internal class Person
+    {
+        static int DEFAULT_HEIGHT = 5;
+        static int DEFAULT_WEIGHT = 50;
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double Height { get; set; } = DEFAULT_HEIGHT;
+
+        public double Weight { get; set; } = DEFAULT_WEIGHT;
+
+        public Person(int id , string name , int age) { 
+            Id = id;    
+            Name = name;
+            Age = age;
+        
+        }
+
+        public Person(int id , string name , int age  , double height , double weight ):this( id , name , age) 
+        { 
+            Weight = weight;
+            Height = height;
+            
+        }
+
+        public double CalcuateTheBmi() {
+
+            double bmi = Weight / (Height * Height);  
+
+            return Math.Round(bmi, 2);
+
+        }
+
+        public string GetBodyType(double bmi)
+        {
+            if (bmi <= 18.4)
+                return "Underweight";
+            else if (bmi > 18.4 && bmi < 25.0)
+                return "Normal";
+            else if (bmi >= 25.0 && bmi < 40.0)
+                return "Overweight";
+            else
+                return "Obese";
+                    
+        }
+
+        public override string ToString() {
+            return $"Id : {Id} \nName : {Name} \nAge : {Age} \nHeight : {Height} \nWeight : {Weight}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BmiCalculatorApp.Models;
+
+namespace BmiCalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Samuel", 21);
+
+            Person person2 = new Person(2, "Saroj", 22, 1.75, 150);
+
+
+            Console.WriteLine(person1);
+            
+            
+            double person1Bmi = person1.CalcuateTheBmi();
+            Console.WriteLine($"BMI : {person1Bmi} \nBody Type : {person1.GetBodyType(person1Bmi)}" );
+
+            Console.WriteLine("\n===================================================");
+            Console.WriteLine("\n" + person2);
+
+            double person2Bmi = person2.CalcuateTheBmi();
+            Console.WriteLine($"BMI : {person2Bmi} \nBody Type : {person2.GetBodyType(person2Bmi)}");
+
+
+
+        }
+    }
+}
